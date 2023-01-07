90393d07c8e37328ddde25ee9a46d4078e181fb7
Gustavo Garcia <tavo@hey.com>
2023-01-07 12:21:55 +0000

    ADD: Request class to handle all requests

--- lib/google_analytics_v4_api.rb
+++ lib/google_analytics_v4_api.rb
@@ -4,5 +4,6 @@ module GoogleAnalyticsV4Api
   autoload :Client, "google_analytics_v4_api/client"
   autoload :Account, "google_analytics_v4_api/account"
   autoload :Property, "google_analytics_v4_api/property"
+  autoload :Request, "google_analytics_v4_api/request"
   autoload :Error, "google_analytics_v4_api/error"
 end
--- lib/google_analytics_v4_api/client.rb
+++ lib/google_analytics_v4_api/client.rb
@@ -1,20 +1,14 @@
 # frozen_string_literal: true
 
-require 'json'
-require "net/http"
-require "uri"
-
 module GoogleAnalyticsV4Api
   class Client
-
-    BASE_URL = "https://analyticsadmin.googleapis.com/v1beta"
 
     def initialize(access_token)
       @access_token = access_token
     end
 
     def accounts
-      @accounts ||= GoogleAnalyticsV4Api::Account.parse_list(self, get("/accounts").body)
+      @accounts ||= GoogleAnalyticsV4Api::Account.parse_list(self, get("/accounts"))
     end
 
     def account(account_name)
@@ -22,7 +16,10 @@ module GoogleAnalyticsV4Api
     end
 
     def properties(account_name)
-      @properties ||= GoogleAnalyticsV4Api::Property.parse_list(self, get("/properties", { filter: "parent:#{account_name}"}).body)
+      @properties ||= Hash.new do |h, key|
+        h[key] = GoogleAnalyticsV4Api::Property.parse_list(self, get("/properties", { filter: "parent:#{key}"}))
+      end
+      @properties[account_name]
     end
 
     def property(property_name)
@@ -31,25 +28,13 @@ module GoogleAnalyticsV4Api
         return property unless property.nil?
       end
 
-      GoogleAnalyticsV4Api::Property.parse(self, get("/#{property_name}").body)
+      GoogleAnalyticsV4Api::Property.parse(self, get("/#{property_name}"))
     end
 
     private
     def get(path, params = {})
-      url = "#{BASE_URL}#{path}"
-      url += "?#{URI.encode_www_form params}" unless params.empty?
-      uri = URI(url)
-
-      response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
-        request = Net::HTTP::Get.new uri
-        request["Authorization"] = "Bearer #{@access_token}"
-        request["Content-Type"] = "application/json"
-
-        http.request request
-      end
-      raise GoogleAnalyticsV4Api::Error.new(response) unless response.is_a?(Net::HTTPSuccess)
-
-      response
+      Request.get(access_token: @access_token, path: path, params: params).body
     end
+
   end
 end
