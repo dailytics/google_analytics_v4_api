ac4a8a868f19737ff0a3ec45606f977129b36cb7
Gustavo Garcia <tavo@hey.com>
2023-01-23 15:02:15 +0000

    ADD: limit and offset parameters to reports

--- lib/google_analytics_v4_api/report.rb
+++ lib/google_analytics_v4_api/report.rb
@@ -7,11 +7,13 @@ module GoogleAnalyticsV4Api
   class Report
     attr_accessor :date_ranges, :dimensions, :metrics, :dimension_filter
 
-    def initialize(start_date: nil, end_date: nil, dimensions: [], metrics: [], dimension_filter: {})
+    def initialize(start_date: nil, end_date: nil, dimensions: [], metrics: [], dimension_filter: {}, limit: 20, offset: 0)
       @date_ranges = [{ "startDate": start_date || (Date.today - 30), "endDate": end_date || Date.today }]
-      @dimensions = dimensions.map { |dimension| { "name": dimension } }
+      @dimensions  = dimensions.map { |dimension| { "name": dimension } }
       @metrics = metrics.map { |metric| { "name": metric } }
       @dimension_filter = dimension_filter
+      @limit = limit
+      @offset = offset
     end
 
     def to_json
@@ -20,6 +22,8 @@ module GoogleAnalyticsV4Api
       output[:dimensions] = @dimensions if !@dimensions.nil? && @dimensions.any?
       output[:metrics] = @metrics if !@metrics.nil? && @metrics.any?
       output[:dimensionFilter] = @dimension_filter if !@dimension_filter.nil? && @dimension_filter.any?
+      output[:limit] = @limit unless @limit.nil?
+      output[:offset] = @offset unless @offset.nil?
       output.to_json
     end
   end
